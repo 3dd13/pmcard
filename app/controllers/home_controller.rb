1226a50f3aadebb3cab231b818d92096c4687aa7
sophie <bmzheng@ymail.com>
2014-01-18 21:45:10 +0800

    improve the page experience

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @card_1 = Card.find(1)
-    @card_2 = Card.find(2)
-    @card_3 = Card.find(3)
+    @card = Card.all.order({created_at: :desc}).limit(3)
   end
 end
